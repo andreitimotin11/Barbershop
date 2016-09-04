32496c85cfd4525ebc89a16452bb5977857f8045
Andrei Timotin <andreitimotin11@gmail.com>
2016-09-04 22:15:30 +0300

    am adaugat 2 blocuri

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 	<header class="main-header">
-		<div class="container">
+		<div class="container clearfix">
 			<nav class="main-navigation">
 				<ul>
 					<li>
@@ -41,7 +41,7 @@
 		<div class="index-logo">
 			<img src="img/index-logo.png" width="368" height="204" alt="Barbershop Borodinskiy">
 		</div>
-		<section class="features">
+		<section class="features clearfix">
 			<div class="features-item">
 				<b class="features-name">Быстро</b>
 				<p>Мы делаем свою работу быстро! Два часа пролетят незаметно и вы - счастливый
@@ -79,13 +79,59 @@
 			</div>
 			<div class="index-content-right">
 				<p class="index-content-title">Фотогалерия</p>
-					<img src="" alt="">
-					<a href="#" class="btn"></a>
-					<a href="#" class="btn"></a>
+					<img src="img/photo-gallery.png" alt="barbershop-photo">
+					<a href="#" class="btn">Назад</a>
+					<a href="#" class="btn">Вперед</a>
+			</div>
+		</div>
+		<div class="index-content">
+			<div class="index-content-left">
+				<p class="index-content-title">Контактная информация</p>
+				<p>
+					БАРБЕРШОП «БОРОДИНСКИЙ»<br>
+					АДРЕС: Г. САНКТ-ПЕТЕРБУРГ, Б. КОНЮШЕННАЯ, Д. 19/8<br>
+					ТЕЛЕФОН: +7 (495) 666-02-66
+				</p>
+				<p>
+					ВРЕМЯ РАБОТЫ:<br>
+					ПН — ПТ: С 10:00 ДО 22:00<br>
+					СБ — ВС: С 10:00 ДО 19:00
+				</p>
+
+				<a href="#" class="btn">Как проехать</a>
+				<a href="#" class="btn">Обратная связь</a>
+
+			</div>
+			<div class="index-content-right">
+				<p class="index-content-title">Записаться</p>
+				<form action="index.php" class="register" name="form">
+					<input type="text" name="" value="Дата" >
+					<input type="text" name="" value="Время" >
+					<input type="text" name="" value="Ваше имя">
+					<input type="text" name="" value="Телефон">
+					<button type="submit" value="Отправить"></button>
+				</form>
 			</div>
 		</div>
 	</main>
+	<footer class="main-footer container">
+		<section>
+			БАРБЕРШОП «БОРОДИНСКИЙ»<br>
+			АДРЕС: Г. САНКТ-ПЕТЕРБУРГ, УЛ. Б. КОНЮШЕННАЯ, Д. 19/8<br>
+			<a href="#">КАК НАС НАЙТИ?</a>
+		</section>
+		<section>
+			<p>Давайте дружить!</p>
+			<a href="#" class="social-btn">vkontakte<img src="img/vk.png" alt="vkontakte"></a>
+			<a href="#" class="social-btn">facebook<img src="img/fb.png" alt="facebook"></a>
+			<a href="#" class="social-btn">instagram<img src="img/instagram.png" alt="instagram"></a>
+		</section>
+		<section>
+			<p>Разработано:</p>
+			<a href="htmlacademy.ru">htmlacademy.ru</a>
+		</section>
 
-
+	</footer>
+<br><br><br><br><br>
 </body>
 </html>
